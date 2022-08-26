ccf2550526bbfc43df56bdb892cc9746fe4fffe0
Eduardo Marin <du.marin10@gmail.com>
2022-08-25 23:03:40 -0300

    confirm JS

---
+++ app.js
@@ -0,0 +1,12 @@
+let resposta = window.confirm('Clique em qualquer botão');
+
+if(resposta == true){
+
+    document.write('você escolheu o botão <b>OK</b> !!!');
+
+}
+else{
+
+    document.write('você escolheu o botão <b>CANCELAR</b> !!!');
+ 
+}
